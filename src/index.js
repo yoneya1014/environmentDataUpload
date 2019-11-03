ed853ab41a4738a8bf3e28f61c1cb95c3c18ef74
yoneya1014 <yoneya1014@gmail.com>
2019-11-03 23:45:27 +0900

    センサー側のプログラムと合成

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require('dotenv/config');
 require('date-utils');
 const admin = require('firebase-admin');
 const getData = require('./modules/getData');
+const exec = require('child_process').execSync;
 
 admin.initializeApp({
     credential: admin.credential.cert(require('../config/serviceAccountKey')),
@@ -13,13 +14,14 @@ const database = admin.database();
 function uploadData() {
     const ref = database.ref('data' + String(process.env.SENSOR_NUMBER));
     const date = new Date();
+    getData.setData(JSON.parse(exec('python3 /home/pi/environmentDataUpload/src/i2c/i2c.py').toString()));
     const data = {
-        "templature": getData.getTemplature(),
+        "temperature": getData.getTemperature(),
         "humidity": getData.getHumidity(),
         /*"solihumidity": getData.getSoliHumidity(),
         "barometricpressure": getData.getBarometricPressure(),
-        "illuminance": getData.getIlluminance(),
-        "timestamp": date.toFormat("YYYY年MM月DD日HH24時MI分SS秒")*/
+        "illuminance": getData.getIlluminance(),*/
+        "timestamp": date.toFormat("YYYY年MM月DD日HH24時MI分SS秒")
     };
     console.log(data);
     ref.set(data)
